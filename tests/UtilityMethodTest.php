1b64a6e5ce069712731893b537337749382d7eff
uniibu <unibtc@gmail.com>
2017-04-12 08:45:18 +0000

    update

--- tests/UtilityMethodTest.php
+++ tests/UtilityMethodTest.php
@@ -10,7 +10,7 @@ namespace Nbbc\Tests;
 
 use Nbbc\BBCode;
 
-class UtilityMethodTest extends \PHPUnit_Framework_TestCase {
+class UtilityMethodTest extends \PHPUnit\Framework\TestCase {
     /**
      * Provide data for {@link testIsValidTLDValid()} that should be valid.
      *
